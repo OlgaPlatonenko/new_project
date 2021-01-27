fe592fb5db4a36321663659cf3c7e953b80dabc2
Olga <platonenkoolga@gmail.com>
2021-01-27 16:43:59 +0300

    first

---
+++ js/script.js
@@ -0,0 +1,12 @@
+"use strict"; 
+//const res = confirm("ffff");
+const sss = prompt("djfvkjn","qwerty");
+console.log(sss);
+
+const answer = [];
+answer[0] = prompt("1");
+answer[1] = prompt("21");
+console.log(answer);
+
+document.write(answer);
+ 
